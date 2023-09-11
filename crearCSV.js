0bd1061f14465b1b7b76f682ac9afd0361aa4ea4
Marcos Perez <perezmarcos04@yahoo.com.ar>
2023-09-11 17:05:10 -0300

    Agrego función para descargar archivo CSV

--- crearCSV.js
+++ crearCSV.js
@@ -1,6 +1,10 @@
-// Encabezado del archivo CSV
+let csv = [];
+let fileTitle = "reporteGenerado";
 
+
+//Función de conversión de JSON a CSV
 function pasarJSONaCSV() {
+    // Encabezado del archivo CSV
     let campos = ["First Name", "Last name", "Email", "Curriculum/Series Title", "Completed Courses", "Courses in Curriculum"];
 
     // Función para reemplazar un valor null con un string vacío.
@@ -11,7 +15,7 @@ function pasarJSONaCSV() {
     // Dado que los datos vienen en un JSON llamado "sortedData", tomo cada elemento y le aplico la función
     // replacer para convertir a string cada campo que se definió arriba.
     // Luego al final concateno cada elemento del array usando comas.
-    let csv = sortedData.map(function(row) {
+    csv = sortedData.map(function(row) {
         return campos.map(function(fieldName){
           return JSON.stringify(row[fieldName], replacer)
         }).join(',')
@@ -25,7 +29,29 @@ function pasarJSONaCSV() {
     // Finalmente uno cada elemento del array del CSV con caracteres de retorno de carro y nueva línea.
     csv = csv.join('\r\n');
     
-    console.log(csv)
+    //console.log(csv)
+}
+
+// Función de exportado de CSV en navegador a un archivo en disco rígido
+function exportarArchivo() {
+    let exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+
+    let blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, exportedFilenmae);
+    } else {
+        let link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", exportedFilenmae);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
 }
 
 
